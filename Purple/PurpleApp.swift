ac57c0f94527f493076dfa5aeafb152767291c84
“dnzhelloo” <dnzhelloo@icloud.com>
2023-07-25 19:30:33 +0300

    Initial Commit

---
+++ Purple/PurpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurpleApp.swift
+//  Purple
+//
+//  Created by Work on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PurpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
